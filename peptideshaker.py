476c30a9cb7a453292f8394eb99c6d3413e96009
Bjoern Gruening <bjoern.gruening@gmail.com>
2014-04-11 20:32:16 +0200

    use subclassing data types

--- peptideshaker.py
+++
@@ -1,8 +0,0 @@
-from galaxy.datatypes.binary import Binary
-
-
-class Cps(Binary):
-    """Class describing a PeptideShaker CPS files"""
-    file_ext = "cps"
-
-Binary.register_unsniffable_binary_ext('cps')
