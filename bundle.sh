52fcf4c1a534b2d30a792bbb7f1367d2f6700294
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-01-31 12:46:26 +0100

    remove unuesed files

--- bundle.sh
+++
@@ -1,1 +0,0 @@
-tar --exclude=.DS_Store --exclude=.hgcheck --exclude=.hg --exclude=*.tar --exclude=*.bz2 --exclude=admin_scripts --exclude=bundle.sh -cvf peptideshaker.tar ./;bzip2 -f peptideshaker.tar
