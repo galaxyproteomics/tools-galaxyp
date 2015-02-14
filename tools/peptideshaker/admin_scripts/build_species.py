6333bc10df3146568b71336658fd57b867de3e39
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-02-14 20:15:26 +0100

    add new trival script to create the species select menues

---
+++ tools/peptideshaker/admin_scripts/build_species.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+for line in open( sys.argv[1] ):
+    line = line.strip()
+    if line and not line.startswith('>'):
+        name, id = line.split('\t')
+        line = '<option value="%s">%s</option>' % (name, name)
+    print line
