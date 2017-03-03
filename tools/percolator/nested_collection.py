cc18013c0037cca198576aba707cd3cd6493b84b
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-03-03 22:19:00 +0100

    Better help texts, variable names and labels for the xml and py wrapper

--- tools/percolator/nested_collection.py
+++ tools/percolator/nested_collection.py
@@ -4,22 +4,22 @@ import re
 from collections import OrderedDict
 
 
-def get_filename_index_with_identifier(spectrafiles, pool_id):
+def get_filename_index_with_identifier(realnames, pool_id):
     pool_indices = []
-    for index, fn in enumerate(spectrafiles):
+    for index, fn in enumerate(realnames):
         if re.search(pool_id, fn) is not None:
             pool_indices.append(index)
     return pool_indices
 
 
-def get_perco_batches_from_spectrafiles(spectrafiles, batchsize, ppool_ids):
-    """For an amount of input spectra files, pool identifiers and a batch size,
-    return batches of files that can be percolated together"""
-    if ppool_ids:
+def get_batches_of_galaxyfiles(realnames, batchsize, pool_ids):
+    """For an amount of input files, pool identifiers and a batch size,
+    return batches of files for a list of lists"""
+    if pool_ids:
         filegroups = OrderedDict([(p_id, get_filename_index_with_identifier(
-                                   spectrafiles, p_id)) for p_id in ppool_ids])
+                                   realnames, p_id)) for p_id in pool_ids])
     else:
-        filegroups = {1: range(len(spectrafiles))}
+        filegroups = {1: range(len(realnames))}
     batch = []
     for grouped_indices in filegroups.values():
         for index in grouped_indices:
@@ -35,14 +35,14 @@ def get_perco_batches_from_spectrafiles(spectrafiles, batchsize, ppool_ids):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batchsize', dest='batchsize')
-    parser.add_argument('--spectrafiles', dest='spectrafiles', nargs='+')
-    parser.add_argument('--searchfiles', dest='searchfiles', nargs='+')
-    parser.add_argument('--percolator-pool-ids', dest='percopoolids', nargs='+', default=False)
+    parser.add_argument('--real-names', dest='realnames', nargs='+')
+    parser.add_argument('--galaxy-files', dest='galaxyfiles', nargs='+')
+    parser.add_argument('--pool-ids', dest='poolids', nargs='+', default=False)
     args = parser.parse_args()
-    for batchcount, batch in enumerate(get_perco_batches_from_spectrafiles(
-            args.spectrafiles, args.batchsize, args.percopoolids)):
-        for fncount, batchfile in enumerate([args.searchfiles[index] for index in batch]):
-            os.symlink(batchfile, 'ppool{}_mzidfn{}.mzid'.format(batchcount, fncount))
+    for batchcount, batch in enumerate(get_batches_of_galaxyfiles(
+            args.realnames, args.batchsize, args.poolids)):
+        for fncount, batchfile in enumerate([args.galaxyfiles[index] for index in batch]):
+            os.symlink(batchfile, 'pool{}_inputfn{}.mzid'.format(batchcount, fncount))
 
 
 if __name__ == '__main__':
