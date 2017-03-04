3f4ef323422740f984a83b1e5cde42019fa2d901
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-03-04 20:57:02 +0100

    Nested collections also output the pool_id filename pattern in each dataset. Possibility to not use batchsize, only pool_id, in which case the batches become as large as the amount of datasets in the pool.

--- tools/percolator/nested_collection.py
+++ tools/percolator/nested_collection.py
@@ -21,29 +21,32 @@ def get_batches_of_galaxyfiles(realnames, batchsize, pool_ids):
     else:
         filegroups = {1: range(len(realnames))}
     batch = []
-    for grouped_indices in filegroups.values():
+    for pool_id, grouped_indices in filegroups.items():
+        if pool_id == 1:
+            pool_id = 'pool0'
         for index in grouped_indices:
             batch.append(index)
-            if len(batch) == int(batchsize):
-                yield batch
+            if batchsize and len(batch) == int(batchsize):
+                yield pool_id, batch
                 batch = []
         if len(batch) > 0:
-            yield batch
+            yield pool_id, batch
             batch = []
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batchsize', dest='batchsize')
+    parser.add_argument('--batchsize', dest='batchsize', default=False)
     parser.add_argument('--real-names', dest='realnames', nargs='+')
     parser.add_argument('--galaxy-files', dest='galaxyfiles', nargs='+')
     parser.add_argument('--pool-ids', dest='poolids', nargs='+', default=False)
     args = parser.parse_args()
-    for batchcount, batch in enumerate(get_batches_of_galaxyfiles(
+    for batchcount, (pool_id, batch) in enumerate(get_batches_of_galaxyfiles(
             args.realnames, args.batchsize, args.poolids)):
         for fncount, batchfile in enumerate([args.galaxyfiles[index] for index in batch]):
-            os.symlink(batchfile, 'pool{}_inputfn{}.mzid'.format(batchcount, fncount))
-
+            dsetname = '{}___batch{}_inputfn{}.mzid'.format(pool_id, batchcount, fncount)
+            print('producing', dsetname)
+            os.symlink(batchfile, dsetname)
 
 if __name__ == '__main__':
     main()
