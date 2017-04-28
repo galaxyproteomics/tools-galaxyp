2d59c17d8b0b36e56d179c836df43adfaec3b8a4
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-04-28 09:36:38 +0200

    Include display_name of file when nesting collection so it can be tracked instead of using "inputfn0"

--- tools/percolator/nested_collection.py
+++ tools/percolator/nested_collection.py
@@ -20,18 +20,19 @@ def get_batches_of_galaxyfiles(realnames, batchsize, pool_ids):
                                    realnames, p_id)) for p_id in pool_ids])
     else:
         filegroups = {1: range(len(realnames))}
-    batch = []
+    batch, in_pool_indices = [], []
     for pool_id, grouped_indices in filegroups.items():
         if pool_id == 1:
             pool_id = 'pool0'
-        for index in grouped_indices:
-            batch.append(index)
+        for in_pool_index, total_index in enumerate(grouped_indices):
+            batch.append(total_index)
+            in_pool_indices.append(in_pool_index)
             if batchsize and len(batch) == int(batchsize):
-                yield pool_id, batch
-                batch = []
+                yield pool_id, batch, in_pool_indices
+                batch, in_pool_indices = [], []
         if len(batch) > 0:
-            yield pool_id, batch
-            batch = []
+            yield pool_id, batch, in_pool_indices
+            batch, in_pool_indices = [], []
 
 
 def main():
@@ -41,12 +42,12 @@ def main():
     parser.add_argument('--galaxy-files', dest='galaxyfiles', nargs='+')
     parser.add_argument('--pool-ids', dest='poolids', nargs='+', default=False)
     args = parser.parse_args()
-    for batchcount, (pool_id, batch) in enumerate(get_batches_of_galaxyfiles(
+    for batchcount, (pool_id, batch, in_pool_indices) in enumerate(get_batches_of_galaxyfiles(
             args.realnames, args.batchsize, args.poolids)):
-        for fncount, batchfile in enumerate([args.galaxyfiles[index] for index in batch]):
-            dsetname = '{}_batch{}___inputfn{}.data'.format(pool_id, batchcount, fncount)
+        for fnindex, in_pool_index in zip(batch, in_pool_indices):
+            dsetname = '{}_batch{}___inputfn{}_{}.data'.format(pool_id, batchcount, in_pool_index, args.realnames[fnindex])
             print('producing', dsetname)
-            os.symlink(batchfile, dsetname)
+            os.symlink(args.galaxyfiles[fnindex], dsetname)
 
 if __name__ == '__main__':
     main()
