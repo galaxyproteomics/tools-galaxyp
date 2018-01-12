56be828b88968dce4ee5774d8eed3de73384b0a9
NguyenLien <thi-phuong-lien.nguyen@etu.univ-amu.fr>
2018-01-12 15:13:13 +0100

    fix wrong output filter kw and value #59

--- filter_keywords_values/filter_kw_val.py
+++ filter_keywords_values/filter_kw_val.py
@@ -3,6 +3,9 @@ import re
 
 
 def options():
+    """
+    Parse options
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="Input file", required=True)
     parser.add_argument("-m", "--match", help="Exact macth")
@@ -16,28 +19,35 @@ def options():
 
     filters(args)
 
-    # python filter2.py -i "/projet/galaxydev/galaxy/tools/proteore_uc1/proteinGroups_Maud.txt" --protein_IDs "A2A288:A8K2U0" --peptides 2 "=" -o "test-data/output_MQfilter.txt"
-    
+    # python filter2.py -i "/projet/galaxydev/galaxy/tools/proteore_uc1/proteinGroups_Maud.txt"
+    # --protein_IDs "A2A288:A8K2U0" --peptides 2 "=" -o "test-data/output_MQfilter.txt"
 
-def isnumber(format, n):
+
+def isnumber(number_format, n):
+    """
+    Check if a variable is a float or an integer
+    """
     float_format = re.compile("^[\-]?[1-9][0-9]*\.?[0-9]+$")
     int_format = re.compile("^[\-]?[1-9][0-9]*$")
     test = ""
-    if format == "int":
+    if number_format == "int":
         test = re.match(int_format, n)
-    elif format == "float":
+    elif number_format == "float":
         test = re.match(float_format, n)
     if test:
         return True
-    else:
-        return False
+#    else:
+#        return False
 
 def filters(args):
+    """
+    Filter the document
+    """
     MQfilename = args.input.split(",")[0]
     header = args.input.split(",")[1]
     MQfile = readMQ(MQfilename)
     results = [MQfile, None]
-    
+
     if args.kw:
         keywords = args.kw
         for k in keywords:
@@ -56,20 +66,20 @@ def filters(args):
 
     # Write results to output
     output = open(args.output, "w")
-    output.write("".join(results[0]))
+    output.write("\n".join(results[0]))
     output.close()
 
     # Write deleted lines to trash_file
     trash = open(args.trash_file, "w")
     #print("".join(results[1]))
-    trash.write("".join(results[1]))
+    trash.write("\n".join(results[1]))
     trash.close()
 
 def readOption(filename):
     f = open(filename, "r")
-    file = f.read()
+    file_content = f.read()
     #print(file)
-    filter_list = file.split("\n")
+    filter_list = file_content.split("\n")
     #print(filter_list)
     filters = ""
     for i in filter_list:
@@ -83,38 +93,42 @@ def readMQ(MQfilename):
     mqfile = open(MQfilename, "r")
     mq = mqfile.readlines()
     # Remove empty lines (contain only space or new line or "")
-    [mq.remove(blank) for blank in mq if blank.isspace() or blank == ""]     
+    [mq.remove(blank) for blank in mq if blank.isspace() or blank == ""]
     return mq
-    
+
 def filter_keyword(MQfile, header, filtered_lines, ids, ncol, match):
     mq = MQfile
     if isnumber("int", ncol.replace("c", "")):
         id_index = int(ncol.replace("c", "")) - 1 #columns.index("Majority protein IDs")
     else:
-        raise ValueError("Please specify the column where you would like to apply the filter with valid format")
-            
+        raise ValueError("Please specify the column where "
+                         "you would like to apply the filter "
+                         "with valid format")
+
     ids = ids.upper().split(":")
     [ids.remove(blank) for blank in ids if blank.isspace() or blank == ""]
-    
+
     if header == "true":
         header = mq[0]
         content = mq[1:]
     else:
         header = ""
         content = mq[:]
-    
+
     if not filtered_lines: # In case there is already some filtered lines from other filters
         filtered_lines = []
         if header != "":
             filtered_lines.append(header)
 
-    for line in content:    
+    for line in content:
+        line = line.replace("\n", "")
         id_inline = line.split("\t")[id_index].replace('"', "").split(";")
         one_id_line = line.replace(line.split("\t")[id_index], id_inline[0]) # Take only first IDs
-        
+        line = line + "\n"
+
         if match != "false":
             # Filter protein IDs
-            if any (pid.upper() in ids for pid in id_inline):
+            if any(pid.upper() in ids for pid in id_inline):
                 #ids = prot_ids.split(":")
                 #print(prot_ids.split(":"))
                 #if prot_id in ids:
@@ -123,55 +137,55 @@ def filter_keyword(MQfile, header, filtered_lines, ids, ncol, match):
             else:
                 mq[mq.index(line)] = one_id_line
         else:
-            if any (ft in pid.upper() for pid in id_inline for ft in ids):
+            if any(ft in pid.upper() for pid in id_inline for ft in ids):
                 filtered_lines.append(one_id_line)
                 mq.remove(line)
             else:
                 mq[mq.index(line)] = one_id_line
     return mq, filtered_lines
-    
+
 def filter_value(MQfile, header, filtered_prots, filter_value, ncol, opt):
     mq = MQfile
     if ncol and isnumber("int", ncol.replace("c", "")): #"Gene names" in columns:
         index = int(ncol.replace("c", "")) - 1 #columns.index("Gene names")
     else:
-        raise ValueError("Please specify the column where you would like to apply the filter with valid format")
-        
+        raise ValueError("Please specify the column where "
+                         "you would like to apply the filter "
+                         "with valid format")
     if header == "true":
         header = mq[0]
         content = mq[1:]
     else:
         header = ""
         content = mq[:]
-    
     if not filtered_prots: # In case there is already some filtered lines from other filters
         filtered_prots = []
         if header != "":
             filtered_prots.append(header)
-        
+
     for prot in content:
         filter_value = float(filter_value)
         pep = prot.split("\t")[index].replace('"', "")
         if pep.replace(".", "", 1).isdigit():
             if opt == "<":
-                if not float(pep) < filter_value:
+                if float(pep) >= filter_value:
                     filtered_prots.append(prot)
                     mq.remove(prot)
             elif opt == "<=":
-                if not float(pep) <= filter_value:
+                if float(pep) > filter_value:
                     filtered_prots.append(prot)
                     mq.remove(prot)
             elif opt == ">":
             #print(prot.number_of_prots, filter_value, int(prot.number_of_prots) > filter_value)
-                if not float(pep) > filter_value:
+                if float(pep) <= filter_value:
                     filtered_prots.append(prot)
                     mq.remove(prot)
             elif opt == ">=":
-                if not float(pep) >= filter_value:
+                if float(pep) < filter_value:
                     filtered_prots.append(prot)
                     mq.remove(prot)
             else:
-                if not float(pep) == filter_value:
+                if float(pep) != filter_value:
                     filtered_prots.append(prot)
                     mq.remove(prot)
     return mq, filtered_prots #output, trash_file
