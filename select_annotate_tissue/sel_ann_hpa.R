f768af0e97f8c2a785e15bfa0cae519dcd836863
NguyenLien <thi-phuong-lien.nguyen@etu.univ-amu.fr>
2018-01-16 14:12:21 +0100

    update HPA Tumor tissue expression level by tissue

--- select_annotate_tissue/sel_ann_hpa.R
+++ select_annotate_tissue/sel_ann_hpa.R
@@ -1,11 +1,13 @@
 
-# Read file and return file content as data.frame?
+# Read file and return file content as data.frame
 readfile = function(filename, header) {
   if (header == "true") {
     # Read only first line of the file as header:
     headers <- read.table(filename, nrows = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
     #Read the data of the files (skipping the first row):
     file <- read.table(filename, skip = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+    # Remove empty rows
+    file <- file[!apply(is.na(file) | file == "", 1, all),]
     #And assign the header to the data:
     names(file) <- headers
   }
@@ -16,17 +18,12 @@ readfile = function(filename, header) {
 }
 
 
-# input has to be a list of ID in ENSG format
+# input has to be a list of IDs in ENSG format
 # tissue is one of unique(HPA.normal.tissue$Tissue)
 # level is one, or several, or 0 (=ALL) of "Not detected", "Medium", "High", "Low"
 # reliability is one, or several, or 0 (=ALL) of "Approved", "Supported", "Uncertain"
 annot.HPAnorm<-function(input, HPA_normal_tissue, tissue, level, reliability, not_mapped_option) {
-  
-  #print(HPA_normal_tissue[1:10,]$Gene)
-  #print("ENSG00000211638" %in% HPA_normal_tissue$Gene)
-  #print(gsub('"', "",HPA_normal_tissue$Gene) %in% gsub('"', "",input))
   dat <- subset(HPA_normal_tissue, Gene %in% input)
-  #print(dat)
   
   if (length(tissue)==1) { 
     res.Tissue<-subset(dat, Tissue==tissue) 
@@ -34,7 +31,6 @@ annot.HPAnorm<-function(input, HPA_normal_tissue, tissue, level, reliability, no
   if (length(tissue)>1)  { 
     res.Tissue<-subset(dat, Tissue %in% tissue) 
   }
-  
   
   if (length(level)==1) { 
     res.Level<-subset(res.Tissue, Level==level) 
@@ -52,8 +48,6 @@ annot.HPAnorm<-function(input, HPA_normal_tissue, tissue, level, reliability, no
     res.Rel<-subset(res.Level, Reliability %in% reliability) 
   }
   
-  #print(res.Rel)
-  #print(matrix(setdiff(intersect(input, unique(dat$Gene)), unique(res.Rel$Gene))))
   if (not_mapped_option == "true") {
     if (length(setdiff(intersect(input, unique(dat$Gene)), unique(res.Rel$Gene)))>0) {
       not_match_IDs <- matrix(setdiff(intersect(input, unique(dat$Gene)), unique(res.Rel$Gene)), ncol = 1, nrow = length(setdiff(intersect(input, unique(dat$Gene)), unique(res.Rel$Gene))))
@@ -76,11 +70,32 @@ annot.HPAnorm<-function(input, HPA_normal_tissue, tissue, level, reliability, no
     res <- res.Rel
   }
   
-  
-  #print(res)
   return(res)
   
 }
+
+annot.HPAcancer<-function(input, HPA_cancer_tissue, cancer, not_mapped_option) {
+  dat <- subset(HPA_cancer_tissue, Gene %in% input)
+
+  if (length(cancer)==1) { 
+    res.Cancer<-subset(dat, Cancer==cancer) 
+  }
+  if (length(cancer)>1)  { 
+    res.Cancer<-subset(dat, Cancer %in% cancer) 
+  }
+
+  if (not_mapped_option == "true") {
+    not.mapped <- matrix(ncol=ncol(HPA_cancer_tissue)-1, nrow=length(setdiff(input, unique(dat$Gene))))
+    not.mapped <- cbind(matrix(setdiff(input, unique(dat$Gene)), ncol = 1, nrow = length(setdiff(input, unique(dat$Gene)))), unname(not.mapped))
+    colnames(not.mapped) <- colnames(HPA_cancer_tissue)
+    res <- rbind(res.Cancer, not.mapped)
+  }
+  else {
+    res <- res.Cancer
+  }
+  return(res)
+}
+
 
 main <- function() {
   args <- commandArgs(TRUE)
@@ -92,14 +107,18 @@ main <- function() {
   if("--help" %in% args) {
     cat("Selection and Annotation HPA
     Arguments:
-        --ref_file: HPA normal tissue file path
+        --ref_file: HPA normal/cancer tissue file path
         --input_type: type of input (list of id or filename)
-        --input: input
+        --input: list of IDs in ENSG format
         --column_number: the column number which you would like to apply...
         --header: true/false if your file contains a header
-        --tissue: list of tissues
-        --level: Not detected, Low, Medium, High
-        --reliability: Supportive, Uncertain
+        --atlas: normal/cancer
+          if normal:
+            --tissue: list of tissues
+            --level: Not detected, Low, Medium, High
+            --reliability: Supportive, Uncertain
+          if cancer:
+            --cancer: Cancer tissues
         --not_mapped: true/false if your output file should contain not-mapped and not-match IDs 
         --output: output filename \n")
     q(save="no")
@@ -110,8 +129,8 @@ main <- function() {
   argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
   args <- as.list(as.character(argsDF$V2))
   names(args) <- argsDF$V1
-  #print(args)
   
+  # Extract input
   input_type = args$input_type
   if (input_type == "list") {
     list_id = strsplit(args$input, " ")[[1]]
@@ -121,29 +140,39 @@ main <- function() {
     column_number = as.numeric(gsub("c", "" ,args$column_number))
     header = args$header
     file = readfile(filename, header)
-    #print(file)
     list_id = c()
     list_id = sapply(strsplit(file[,column_number], ";"), "[", 1)
-    #list_id = gsub('"', "", list_id)
   }
-  #print(list_id)
   input = list_id
-  #print("ENSG00000211638"%in%input)
-  tissue = strsplit(args$tissue, ",")[[1]]
-  level = strsplit(args$level, ",")[[1]]
-  reliability = strsplit(args$reliability, ",")[[1]]
+
+  # Read reference file
+  reference_file = read.table(args$ref_file, header = TRUE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+  print(colnames(reference_file))
+
+  # Extract other options
+  atlas = args$atlas
   not_mapped_option = args$not_mapped
-  output = args$output
-  
-  # Calculation
-  HPA_normal_tissue = read.table(args$ref_file, header = TRUE, sep = ",", stringsAsFactors = FALSE, fill = TRUE)
-  res = annot.HPAnorm(input, HPA_normal_tissue, tissue, level, reliability, not_mapped_option)
+  if (atlas=="normal") {
+    tissue = strsplit(args$tissue, ",")[[1]]
+    level = strsplit(args$level, ",")[[1]]
+    reliability = strsplit(args$reliability, ",")[[1]]
+    # Calculation
+    res = annot.HPAnorm(input, reference_file, tissue, level, reliability, not_mapped_option)
+  }
+  else if (atlas=="cancer") {
+    cancer = strsplit(args$cancer, ",")[[1]]
+    # Calculation
+    res = annot.HPAcancer(input, reference_file, cancer, not_mapped_option)
+  }
   
   # Write output
+  output = args$output
   write.table(res, output, sep = "\t", quote = FALSE, row.names = FALSE)
 }
 
 main()
 
+# Example commands
+# Rscript sel_ann_hpa.R --input_type="file" --input="./test-data/ENSGid.txt" --ref_file="./pathology.tsv" --cancer="lung cancer,carcinoid" --not_mapped="true" --column_number="c1" --header="true" --output="test-data/ENSG_tissue_output_cancer.txt"
 # Rscript sel_ann_hpa.R --input_type="file" --input="./test-data/ENSGid.txt" --ref_file="./normal_tissue.tsv" --tissue="lung" --level="Not detected,Medium,High,Low" --reliability="Approved,Supported,Uncertain" --column_number="c1" --header="true" --not_mapped="false" --output="./test-data/ENSG_tissue_output.txt"
 # Rscript sel_ann_hpa.R --input_type="file" --input="./test-data/ENSG_no_not_match.txt" --ref_file="/Users/LinCun/Documents/ProteoRE/usecase1/normal_tissue.csv" --tissue="lung" --level="Not detected,Medium,High,Low" --reliability="Approved,Supportive,Uncertain" --column_number="c1" --header="true" --output="./test-data/ENSG_tissue_output2.txt"
