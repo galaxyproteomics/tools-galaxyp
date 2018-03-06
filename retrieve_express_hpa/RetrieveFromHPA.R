5d7ce2286509328f4d100e38c21e5219d3e6dbce
NguyenLien <thi-phuong-lien.nguyen@etu.univ-amu.fr>
2018-03-06 16:30:09 +0100

    new component Retrieve From HPA #71

---
+++ retrieve_express_hpa/RetrieveFromHPA.R
@@ -0,0 +1,75 @@
+
+
+select.HPAimmunohisto<-function(hpa_ref, tissue, level, reliability) {
+  load(hpa_ref)
+  if (tissue == "tissue") {
+    tissue <- unique(HPA.normal$Tissue) 
+  }
+  if (level == "level") {
+    level <- unique(HPA.normal$Level)
+  }
+  if (reliability == "reliability") {
+    reliability <- unique(HPA.normal$Reliability)
+  }
+  res.imm <- subset(HPA.normal, Tissue%in%tissue & Level%in%level & Reliability%in%reliability)
+  return(res.imm)
+}
+
+    
+    
+select.HPARNAseq<-function(hpa_ref, sample) {
+  load(hpa_ref)
+  res.rna <- subset(HPA.rnaTissue, Sample%in%sample)
+  return(res.rna) 
+}
+
+main <- function() {
+  args <- commandArgs(TRUE)
+  if(length(args)<1) {
+    args <- c("--help")
+  }
+  
+  # Help section
+  if("--help" %in% args) {
+    cat("Selection and Annotation HPA
+    Arguments:
+        --data_source: immuno/rnaseq
+          if immuno:
+            --tissue: list of tissues
+            --level: Not detected, Low, Medium, High
+            --reliability: Supported, Approved, Enhanced, Uncertain
+          if rnaseq:
+            --sample: Sample tissues
+        --not_mapped: true/false if your output file should contain not-mapped and not-match IDs 
+        --output: output filename \n")
+    q(save="no")
+  }
+  
+  # Parse arguments
+  parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+  argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
+  args <- as.list(as.character(argsDF$V2))
+  names(args) <- argsDF$V1
+
+  # Extract options
+  data_source = args$data_source
+  hpa_ref = args$hpa_ref
+  if (data_source == "immuno") {
+    tissue = strsplit(args$tissue, ",")[[1]]
+    level = strsplit(args$level, ",")[[1]]
+    reliability = strsplit(args$reliability, ",")[[1]]
+    # Calculation
+    res = select.HPAimmunohisto(hpa_ref, tissue, level, reliability)
+  }
+  else if (data_source == "rnaseq") {
+    sample = strsplit(args$sample, ",")[[1]]
+    # Calculation
+    res = select.HPARNAseq(hpa_ref, sample)
+  }
+
+  # Write output
+  output = args$output
+  write.table(res, output, sep = "\t", quote = FALSE, row.names = FALSE)
+}
+
+main()
