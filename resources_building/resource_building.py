159946055b2eee3bf8b2e8bc71d59ff53b8a893e
NguyenLien <thi-phuong-lien.nguyen@etu.univ-amu.fr>
2018-05-16 14:34:14 +0200

    modify from normal tool to data manager tool resource building

--- resources_building/resource_building.py
+++ resources_building/resource_building.py
@@ -2,10 +2,12 @@
 The purpose of this script is to create source files from different databases to be used in other tools
 """
 
+import os
 import argparse
 import requests
 from io import BytesIO
 from zipfile import ZipFile
+from galaxy.util.json import from_json_string, to_json_string
 
 #######################################################################################################
 # General functions
@@ -28,20 +30,26 @@ def unzip(url, output_file):
 #    - Pathology
 #    - Full Atlas
 #######################################################################################################
-def HPA_sources(tissue):
+def HPA_sources(data_manager_dict, tissue, target_directory):
     if tissue == "HPA_normal_tissue":
+        tissue_name = "HPA normal tissue"
         url = "https://www.proteinatlas.org/download/normal_tissue.tsv.zip"
     elif tissue == "HPA_pathology":
+        tissue_name = "HPA pathology"
         url = "https://www.proteinatlas.org/download/pathology.tsv.zip"
     elif tissue == "HPA_full_atlas":
+        tissue_name = "HPA full atlas"
         url = "https://www.proteinatlas.org/download/proteinatlas.tsv.zip"
     output_file = tissue + ".tsv"
+    path = os.path.join(target_directory, output_file)
     unzip(url, output_file)
+    data_table_entry = dict(value = tissue, name = tissue_name, path = path)
+    _add_data_table_entry(data_manager_dict, data_table_entry)
 
 #######################################################################################################
 # 2. Peptide Atlas
 #######################################################################################################
-def peptide_atlas_sources(tissue):
+def peptide_atlas_sources(data_manager_dict, tissue, target_directory):
     # Define PA Human build released number (here  early 2018)
     atlas_build_id = "472"
     # Define organism_id (here Human) - to be upraded when other organism added to the project
@@ -54,9 +62,20 @@ def peptide_atlas_sources(tissue):
             organism_id + "&sample_category_id=" + sample_category_id + \
             "&QUERY_NAME=AT_GetPeptides&output_mode=tsv&apply_action=QUERY"
     content = requests.get(query)
-    output = open(output_file, "w")
+    tissue_id = "_".join([atlas_build_id, organism_id, sample_category_id])
+    tissue_name = " ".join(tissue.split("-")[1].split("_"))
+    path = os.path.join(target_directory, output_file)
+    output = open(path, "w")
     output.write(content.content)
     output.close()
+    data_table_entry = dict(value = tissue_id, name = tissue_name, path = path)
+    _add_data_table_entry(data_manager_dict, data_table_entry)
+
+def _add_data_table_entry(data_manager_dict, data_table_entry):
+    data_manager_dict['data_tables'] = data_manager_dict.get('data_tables', {})
+    data_manager_dict['data_tables']['proteinatlas'] = data_manager_dict['data_tables'].get('proteinatlas', [])
+    data_manager_dict['data_tables']['proteinatlas'].append(data_table_entry)
+    return data_manager_dict
 
 #######################################################################################################
 # Main function
@@ -65,7 +84,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--hpa", metavar = ("HPA_OPTION"))
     parser.add_argument("--peptideatlas", metavar=("SAMPLE_CATEGORY_ID"))
+    parser.add_argument("-o", "--output")
     args = parser.parse_args()
+
+    data_manager_dict = {}
 
     ## Download source files from HPA
     try:
@@ -73,9 +95,10 @@ def main():
     except NameError:
         hpa = None
     if hpa is not None:
+        target_directory = "test-data/"
         hpa = hpa.split(",")
         for hpa_tissue in hpa:
-            HPA_sources(hpa_tissue)
+            HPA_sources(data_manager_dict, hpa_tissue, target_directory)
     
     ## Download source file from Peptide Atlas query
     try:
@@ -83,9 +106,14 @@ def main():
     except NameError:
         peptide_atlas = None
     if peptide_atlas is not None:
+        target_directory = "test-data/"
         peptide_atlas = peptide_atlas.split(",")
         for pa_tissue in peptide_atlas:
-            peptide_atlas_sources(pa_tissue)
+            peptide_atlas_sources(data_manager_dict, pa_tissue, target_directory)
+ 
+    #save info to json file
+    filename = args.output
+    open(filename, 'wb').write(to_json_string(data_manager_dict))
 
 if __name__ == "__main__":
     main()
