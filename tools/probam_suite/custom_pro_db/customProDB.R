9831b0658efd87e6b7c8bc528d7177672699b1c4
Matt Chambers <matt.chambers42@gmail.com>
2018-05-24 18:58:56 -0500

    Update customProDB.R

--- tools/probam_suite/custom_pro_db/customProDB.R
+++ tools/probam_suite/custom_pro_db/customProDB.R
@@ -100,7 +100,7 @@ customProDB <- function(
     file.symlink(bai_file, paste(bamLink, ".bai", sep=""))
 
     # load customProDB from GitHub (NOTE: downloading the zip is faster than cloning the repo with git2r or devtools::install_github)
-    download.file("https://github.com/chambm/customProDB/archive/50a44bb8c3bc56b9c17f3a1a962f1dfb9963933f", "customProDB.zip", quiet=TRUE)
+    download.file("https://github.com/chambm/customProDB/archive/50a44bb8c3bc56b9c17f3a1a962f1dfb9963933f.zip", "customProDB.zip", quiet=TRUE)
     unzip("customProDB.zip")
     devtools::load_all("customProDB-50a44bb8c3bc56b9c17f3a1a962f1dfb9963933f")
 
