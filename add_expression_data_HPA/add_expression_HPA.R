31fddc161a1ecbec9624c2ac00d7ab5570595f5d
davidchristiany <david.christiany@outlook.com>
2018-11-06 17:03:20 +0100

    add human expression data : blank cells to NA

--- add_expression_data_HPA/add_expression_HPA.R
+++ add_expression_data_HPA/add_expression_HPA.R
@@ -1,21 +1,22 @@
 # Read file and return file content as data.frame
-readfile = function(filename, header) {
-  if (header == "true") {
-    # Read only first line of the file as header:
-    headers <- read.table(filename, nrows = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE, na.strings=c("", "NA"), blank.lines.skip = TRUE, quote = "", comment.char = "")
-    #Read the data of the files (skipping the first row)
-    file <- read.table(filename, skip = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE, na.strings=c("", "NA"), blank.lines.skip = TRUE, quote = "", comment.char = "")
-    # Remove empty rows
-    file <- file[!apply(is.na(file) | file == "", 1, all), , drop=FALSE]
-    #And assign the header to the data
-    names(file) <- headers
+read_file <- function(path,header){
+  file <- try(read.csv(path,header=header, sep="\t",stringsAsFactors = FALSE, quote="\"", check.names = F),silent=TRUE)
+  if (inherits(file,"try-error")){
+    stop("File not found !")
+  }else{
+    return(file)
   }
-  else {
-    file <- read.table(filename, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE, na.strings=c("", "NA"), blank.lines.skip = TRUE, quote = "", comment.char = "")
-    # Remove empty rows
-    file <- file[!apply(is.na(file) | file == "", 1, all), , drop=FALSE]
+}
+
+#convert a string to boolean
+str2bool <- function(x){
+  if (any(is.element(c("t","true"),tolower(x)))){
+    return (TRUE)
+  }else if (any(is.element(c("f","false"),tolower(x)))){
+    return (FALSE)
+  }else{
+    return(NULL)
   }
-  return(file)
 }
 
 add_expression = function(input, atlas, options) {
@@ -63,39 +64,35 @@ main = function() {
   args <- as.list(as.character(argsDF$V2))
   names(args) <- argsDF$V1
 
+  save(args,file="/home/dchristiany/proteore_project/ProteoRE/tools/add_expression_data_HPA/args.rda")
+  #load("/home/dchristiany/proteore_project/ProteoRE/tools/add_expression_data_HPA/args.rda")
+  
   inputtype = args$inputtype
   if (inputtype == "copypaste") {
     input = strsplit(args$input, "[ \t\n]+")[[1]]
-  }
-  else if (inputtype == "tabfile") {
+  } else if (inputtype == "tabfile") {
     filename = args$input
     ncol = args$column
     # Check ncol
     if (! as.numeric(gsub("c", "", ncol)) %% 1 == 0) {
       stop("Please enter an integer for level")
-    }
-    else {
+    } else {
       ncol = as.numeric(gsub("c", "", ncol))
     }
-    header = args$header
-    # Get file content
-    file = readfile(filename, header)
-    # Extract Protein IDs list
-    input = c()
-    for (row in as.character(file[,ncol])) {
-      input = c(input, strsplit(row, ";")[[1]][1])
-    }
+    header = str2bool(args$header)
+    file = read_file(filename, header)
+    input =  unlist(sapply(as.character(file[,ncol]),function(x) rapply(strsplit(x,";"),c),USE.NAMES = FALSE))
   }
 
   # Read protein atlas
   protein_atlas = args$atlas
-  protein_atlas = readfile(protein_atlas, "true")
+  protein_atlas = read_file(protein_atlas, T)
 
   # Add expression
   output = args$output
-  names = c()
   options = strsplit(args$select, ",")[[1]]
   res = add_expression(input, protein_atlas, options)
+  res <- apply(res, c(1,2), function(x) gsub("^$|^ $", NA, x))  #convert "" et " " to NA
 
   # Write output
   if (is.null(res)) {
@@ -103,19 +100,13 @@ main = function() {
   }
   else {
     if (inputtype == "copypaste") {
-      names = c("Ensembl", colnames(res))
-      res = cbind(as.matrix(input), res)
-      colnames(res) = names
-      write.table(res, output, row.names = FALSE, sep = "\t", quote = FALSE)
-    }
-    else if (inputtype == "tabfile") {
-      names = c(names(file), colnames(res))
+      output_content = cbind(as.matrix(input), res)
+      colnames(output_content)[1] = "Ensembl"
+    } else if (inputtype == "tabfile") {
       output_content = cbind(file, res)
-      colnames(output_content) = names
-      write.table(output_content, output, row.names = FALSE, sep = "\t", quote = FALSE)
     }
+  write.table(output_content, output, row.names = FALSE, sep = "\t", quote = FALSE)
   }
-
 }
 
 main()
