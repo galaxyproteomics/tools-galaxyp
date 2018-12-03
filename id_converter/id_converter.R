8b5adceffc1bce6dcc087314b937cdc9a147b6cc
davidchristiany <david.christiany@outlook.com>
2018-12-03 18:00:48 +0100

    id converter : condition for mapping + small changes

--- id_converter/id_converter.R
+++ id_converter/id_converter.R
@@ -20,12 +20,20 @@ str2bool <- function(x){
 
 mapping_ids <- function(list_id,input_id_type,ref_file,options){
   list_id = list_id[!is.na(list_id)]
-  if (length(options) > 1) {
-    res <- data.frame(t(sapply(list_id, function(x) apply(ref_file[grep(x,ref_file[input_id_type][,]),options],2,function(y) paste(y[y!=""],sep="",collapse=";")) )))
-  } else if (length(options)==1){
-    res <- data.frame(sapply(list_id, function(x) gsub(";+$","",paste(ref_file[grep(x,ref_file[input_id_type][,]),options],sep="",collapse=";"))))
-    colnames(res)=options
-  }
+  if (!any(grep(";",ref_file[input_id_type]))) {
+    res <- ref_file[match(list_id,ref_file[input_id_type][,]),c(input_id_type,options)]
+    res=as.data.frame(res)
+    res=res[which(!is.na(res[,1])),]
+    row.names(res)=res[,1]
+    res=res[2:ncol(res)]
+    } else {
+      if (length(options) > 1) {
+        res <- data.frame(t(sapply(list_id, function(x) apply(ref_file[grep(x,ref_file[input_id_type][,]),options],2,function(y) paste(y[y!=""],sep="",collapse=";")) )))
+      } else if (length(options)==1){
+        res <- data.frame(sapply(list_id, function(x) gsub(";+$","",paste(ref_file[grep(x,ref_file[input_id_type][,]),options],sep="",collapse=";"))))
+        colnames(res)=options
+      }
+    }
   
   return (res)
 }
@@ -46,27 +54,42 @@ order_columns <- function (df,ncol){
   return (df)
 }
 
+#take data frame, return  data frame
+split_ids_per_line <- function(line,ncol){
+  
+  #print (line)
+  header = colnames(line)
+  line[ncol] = gsub("[[:blank:]]","",line[ncol])
+  
+  if (length(unlist(strsplit(as.character(line[ncol]),";")))>1) {
+    if (length(line)==1 ) {
+      lines = as.data.frame(unlist(strsplit(as.character(line[ncol]),";")),stringsAsFactors = F)
+    } else {
+        if (ncol==1) {                                #first column
+        lines = suppressWarnings(cbind(unlist(strsplit(as.character(line[ncol]),";")), line[2:length(line)]))
+      } else if (ncol==length(line)) {                 #last column
+        lines = suppressWarnings(cbind(line[1:ncol-1],unlist(strsplit(as.character(line[ncol]),";"))))
+      } else {
+        lines = suppressWarnings(cbind(line[1:ncol-1], unlist(strsplit(as.character(line[ncol]),";"),use.names = F), line[(ncol+1):length(line)]))
+      }
+    }
+    colnames(lines)=header
+    return(lines)
+  } else {
+    return(line)
+  }
+}
+ 
 #create new lines if there's more than one id per cell in the columns in order to have only one id per line
 one_id_one_line <-function(tab,ncol){
-
+  
+  tab[,ncol] = sapply(tab[,ncol],function(x) gsub("[[:blank:]]","",x))
   header=colnames(tab)
-  colnames(tab) = c(sprintf("V%d", seq(1,ncol(tab))))
   res=as.data.frame(matrix(ncol=ncol(tab),nrow=0))
   for (i in 1:nrow(tab) ) {
-    if (length(unlist(strsplit(as.character(tab[i,ncol]),";")))>1){
-      if (ncol==1) {                                #first column
-        res = rbind(res,cbind(unlist(strsplit(as.character(tab[i,ncol]),";")), tab[i,2:ncol(tab)]))
-      } else if (ncol==ncol(tab)) {                 #last column
-        res = rbind(res,cbind(tab[i,1:ncol-1],unlist(strsplit(as.character(tab[i,ncol]),";"))))
-      } else {
-        res = rbind(res,cbind(tab[i,1:ncol-1],unlist(strsplit(as.character(tab[i,ncol]),";")), tab[i,(ncol+1):ncol(tab)]))
-      }
-    } else {
-      res = rbind(res,(tab[i,]))
-    }
+      lines = split_ids_per_line(tab[i,],ncol)
+      res = rbind(res,lines)
   }
-  colnames(res)=header
-  
   return(res)
 }
 
@@ -121,7 +144,7 @@ mapping = function() {
     column_number = as.numeric(gsub("c", "" ,args$column_number))
     header = str2bool(args$header)
     file_all = read_file(filename, header)
-    file_all = suppressWarnings(one_id_one_line(file_all,column_number))
+    file_all = one_id_one_line(file_all,column_number)
     list_id = trimws(gsub("[$,\xc2\xa0]","",sapply(strsplit(as.character(file_all[,column_number]), ";"), "[", 1)))
     # Remove isoform accession number (e.g. "-2")
     list_id = unique(gsub("-.+", "", list_id))
@@ -144,7 +167,9 @@ mapping = function() {
   }
   
   #write output
-  output_content <- data.frame(apply(output_content, c(1,2), function(x) gsub("^$|^ $", NA, x)))
+  header=colnames(output_content)
+  output_content <- as.data.frame(apply(output_content, c(1,2), function(x) gsub("^$|^ $", NA, x)))
+  colnames(output_content)=header
   write.table(output_content, output, row.names = FALSE, sep = '\t', quote = FALSE)
 }
 
