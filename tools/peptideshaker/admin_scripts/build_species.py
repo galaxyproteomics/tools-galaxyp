a02d2f577f29c9c380a6ac7b68d66aab1c38de73
CarlosHorro <carlos.horro@gmail.com>
2020-04-28 11:09:35 +0200

    Avoiding lynting errors for python scripts (3.7).

--- tools/peptideshaker/admin_scripts/build_species.py
+++ tools/peptideshaker/admin_scripts/build_species.py
@@ -7,4 +7,4 @@ for line in open(sys.argv[1]):
     if line and not line.startswith('>'):
         name, id = line.split('\t')
         line = '<option value="%s">%s</option>' % (name, name)
-    print (line)
+    print(line)
