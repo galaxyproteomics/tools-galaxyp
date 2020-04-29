205b7fefb13bbd3f5d7f4c9f37f0363ff86f0dbc
Matthias Bernt <m.bernt@ufz.de>
2020-04-29 13:40:53 +0200

    debug: hardcode -debug 3 for all calls

--- tools/openms/test-data.sh
+++ tools/openms/test-data.sh
@@ -110,13 +110,18 @@ ln -fs TOFCalibration_const test-data/TOFCalibration_const.csv
 echo "Create CTD files"
 conda activate $OPENMSENV
 mkdir -p ctd
+
+# TODO because of https://github.com/OpenMS/OpenMS/issues/4641
+# this needs to be done from within test-data
+cd test-data
 for i in $OPENMSPKG/bin/*
 do
 	b=$(basename $i)
 	echo $b
-	$b -write_ctd ctd/
-	sed -i -e 's/²/^2/' ctd/$b.ctd
+	$b -write_ctd ../ctd/
+	sed -i -e 's/²/^2/' ../ctd/$b.ctd
 done
+cd -
 ###############################################################################
 ## fix ini files: OpenMS test data contains ini files with outdated ini files.
 ## e.g. variables might be in different nodes, outdated variables present, new
