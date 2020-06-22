576ad3d2592e55c0373fe2789b0fe81152356cb5
mariecrane <mcrane@macalester.edu>
2020-06-22 15:05:21 -0500

    Update MT2MQ.R
    Replaced readr functions (read_delim, write_delim) with base R functions (read.delim, write.table)

--- tools/mt2mq/MT2MQ.R
+++ tools/mt2mq/MT2MQ.R
@@ -3,7 +3,6 @@
 # Load libraries
 suppressPackageStartupMessages(library(tidyverse))
 default_locale()
-suppressPackageStartupMessages(library(readr))
 
 # Set parameters from arguments
 args = commandArgs(trailingOnly = TRUE)
@@ -24,9 +23,9 @@ outfile <- args[4]
 
 # Functional mode
 if (mode == "f"){
-  out <- suppressMessages(read_delim(data, "\t", escape_double = FALSE, trim_ws = TRUE)) %>% 
-    filter(!grepl(".+g__.+",`# Gene Family`)) %>% 
-    separate(col=`# Gene Family`, into=c("id", "Extra"), sep=": ", fill="left") %>% 
+  out <- suppressMessages(read.delim(file=data, header=TRUE, sep='\t')) %>% 
+    filter(!grepl(".+g__.+",X..Gene.Family)) %>% 
+    separate(col=X..Gene.Family, into=c("id", "Extra"), sep=": ", fill="left") %>% 
     separate(col=Extra, into = c("namespace", "name"), sep = " ", fill="left", extra="merge") %>% 
     mutate(namespace = if_else(namespace == "[MF]", true = "molecular_function", false = if_else(namespace == "[BP]", true = "biological_process", false = "cellular_component"))) %>% 
     filter(namespace %in% ontology) %>% 
@@ -37,22 +36,22 @@ if (mode == "f"){
 if (mode == "t"){
   files <- dir(path = data)
   out <- tibble(filename = files) %>% 
-    mutate(file_contents= map(filename, ~suppressMessages(read_delim(file.path(data, .),delim = "\t")))) %>% 
+    mutate(file_contents= map(filename, ~suppressMessages(read.delim(file=file.path(data, .), header=TRUE, sep = "\t")))) %>% 
     unnest(cols = c(file_contents)) %>% 
     rename(sample = filename) %>% 
     separate(col = sample, into = c("sample",NA), sep=".tsv") %>% 
     pivot_wider(names_from = sample, values_from = abundance) %>% 
     mutate(rank = "genus") %>% 
     rename(name = genus) %>% 
-    mutate(id = row_number(name)) %>% # filler for taxon id but should eventually find a way to get id from database
+    mutate(id = row_number(name)) %>% # filler for taxon id but should eventually find a way to get id from ncbi database
     select(id, name, rank, 2:ncol(.))
 }
 
 # Function-taxonomy mode
 if (mode == "ft"){
-  out <- suppressMessages(read_delim(data,"\t", escape_double = FALSE, trim_ws = TRUE)) %>% 
-    filter(grepl(".+g__.+",`# Gene Family`)) %>% 
-    separate(col=`# Gene Family`, into=c("id", "Extra"), sep=": ", fill="left") %>% 
+  out <- suppressMessages(read.delim(file=data, header=TRUE, sep='\t')) %>% 
+    filter(grepl(".+g__.+",X..Gene.Family)) %>% 
+    separate(col=X..Gene.Family, into=c("id", "Extra"), sep=": ", fill="left") %>% 
     separate(col=Extra, into = c("namespace", "name"), sep = " ", fill="left", extra="merge") %>% 
     separate(col = name, into = c("name", "taxa"), sep="\\|", extra = "merge") %>%
     separate(col = taxa, into = c("Extra", "genus", "species"), sep = "__") %>% select(-"Extra") %>%
@@ -64,4 +63,4 @@ if (mode == "ft"){
 }
 
 # Write file
-write_delim(x = out, path = outfile, delim = "\t", quote_escape = FALSE);
+write.table(x = out, file = outfile, quote = FALSE, sep = "\t");
