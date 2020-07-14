b812fa608cec71db77b47bc8994e5d1a17203485
mariecrane <mcrane@macalester.edu>
2020-07-14 15:45:25 -0500

    Update MT2MQ.R
    Match style guide to pass the lintr step

--- tools/mt2mq/MT2MQ.R
+++ tools/mt2mq/MT2MQ.R
@@ -3,10 +3,9 @@
 # Load libraries
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(taxize))
-#default_locale()
 
 # Set parameters from arguments
-args = commandArgs(trailingOnly = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 data <- args[1]
   # data: full path to file or directory:
   #   - if in functional or f-t mode, should be a tsv file of HUMAnN2 gene families, after regrouping and renaming to GO, joining samples, and renormalizing to CPM.
@@ -24,43 +23,43 @@ outfile <- args[5]
   # outfile: full path with pathname and extension for output
 
 # Functional mode
-if (mode == "f"){
-  out <- read.delim(file=data, header=TRUE, sep='\t') %>% 
-    filter(!grepl(".+g__.+",X..Gene.Family)) %>% 
-    separate(col=X..Gene.Family, into=c("id", "Extra"), sep=": ", fill="left") %>% 
-    separate(col=Extra, into = c("namespace", "name"), sep = " ", fill="left", extra="merge") %>% 
-    mutate(namespace = if_else(namespace == "[MF]", true = "molecular_function", false = if_else(namespace == "[BP]", true = "biological_process", false = "cellular_component"))) %>% 
-    filter(namespace %in% ontology) %>% 
+if (mode == "f") {
+  out <- read.delim(file = data, header = TRUE, sep = "\t") %>%
+    filter(!grepl(".+g__.+", X..Gene.Family)) %>%
+    separate(col = X..Gene.Family, into = c("id", "Extra"), sep = ": ", fill = "left") %>%
+    separate(col = Extra, into = c("namespace", "name"), sep = " ", fill = "left", extra = "merge") %>%
+    mutate(namespace = if_else(namespace == "[MF]", true = "molecular_function", false = if_else(namespace == "[BP]", true = "biological_process", false = "cellular_component"))) %>%
+    filter(namespace %in% ontology) %>%
     select(id, name, namespace, 4:ncol(.))
 }
 
 # Taxonomic mode
-if (mode == "t"){
+if (mode == "t") {
   files <- dir(path = data)
-  out <- tibble(filename = files) %>% 
-    mutate(file_contents= map(filename, ~read.delim(file=file.path(data, .), header=TRUE, sep = "\t"))) %>% 
-    unnest(cols = c(file_contents)) %>% 
-    rename(sample = filename) %>% 
-    separate(col = sample, into = c("sample",NA), sep=".tsv") %>% 
-    pivot_wider(names_from = sample, values_from = abundance) %>% 
-    mutate(rank = "genus") %>% 
-    rename(name = genus) %>% 
-    mutate(id = get_uid(toString(name), key = api_key, messages=FALSE)) %>%
+  out <- tibble(filename = files) %>%
+    mutate(file_contents = map(filename, ~read.delim(file = file.path(data, .), header = TRUE, sep = "\t"))) %>%
+    unnest(cols = c(file_contents)) %>%
+    rename(sample = filename) %>%
+    separate(col = sample, into = c("sample",NA), sep=".tsv") %>%
+    pivot_wider(names_from = sample, values_from = abundance) %>%
+    mutate(rank = "genus") %>%
+    rename(name = genus) %>%
+    mutate(id = get_uid(toString(name), key = api_key, messages = FALSE)) %>%
     select(id, name, rank, 2:ncol(.))
 }
 
 # Function-taxonomy mode
-if (mode == "ft"){
-  out <- read.delim(file=data, header=TRUE, sep='\t') %>% 
-    filter(grepl(".+g__.+",X..Gene.Family)) %>% 
-    separate(col=X..Gene.Family, into=c("id", "Extra"), sep=": ", fill="left") %>% 
-    separate(col=Extra, into = c("namespace", "name"), sep = " ", fill="left", extra="merge") %>% 
-    separate(col = name, into = c("name", "taxa"), sep="\\|", extra = "merge") %>%
+if (mode == "ft") {
+  out <- read.delim(file = data, header = TRUE, sep = "\t") %>%
+    filter(grepl(".+g__.+", X..Gene.Family)) %>%
+    separate(col = X..Gene.Family, into = c("id", "Extra"), sep = ": ", fill = "left") %>%
+    separate(col = Extra, into = c("namespace", "name"), sep = " ", fill = "left", extra = "merge") %>%
+    separate(col = name, into = c("name", "taxa"), sep = "\\|", extra = "merge") %>%
     separate(col = taxa, into = c("Extra", "genus", "species"), sep = "__") %>% select(-"Extra") %>%
-    mutate_if(is.character, str_replace_all, pattern = "\\.s", replacement = "") %>% 
-    mutate_at(c("species"), str_replace_all, pattern = "_", replacement = " ") %>% 
-    mutate(namespace = if_else(namespace == "[MF]", true = "molecular_function", false = if_else(namespace == "[BP]", true = "biological_process", false = "cellular_component"))) %>% 
-    filter(namespace %in% ontology) %>% 
+    mutate_if(is.character, str_replace_all, pattern = "\\.s", replacement = "") %>%
+    mutate_at(c("species"), str_replace_all, pattern = "_", replacement = " ") %>%
+    mutate(namespace = if_else(namespace == "[MF]", true = "molecular_function", false = if_else(namespace == "[BP]", true = "biological_process", false = "cellular_component"))) %>%
+    filter(namespace %in% ontology) %>%
     select(id, name, namespace, 4:ncol(.))
 }
 
