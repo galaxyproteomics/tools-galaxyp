c91fb59fb38086fbd88669f27df1123ccb00c06d
npinter <npinter@protonmail.com>
2020-12-28 00:36:08 +0100

    Add spaces around infix operators

--- tools/dialignr/dialignr.R
+++ tools/dialignr/dialignr.R
@@ -1,11 +1,11 @@
 library(DIAlignR)
 
-args <- commandArgs(trailingOnly=FALSE)
+args <- commandArgs(trailingOnly = FALSE)
 
 data_path <- paste0(args[7], "/data/")
 runs_file_path <- paste0(args[7], "/runs.txt")
 
-runs <- scan(runs_file_path, what="", sep="\n")
+runs <- scan(runs_file_path, what = "", sep = "\n")
 
 alignTargetedRuns(dataPath = data_path,
                   outFile = "alignedTargetedRuns.csv",
