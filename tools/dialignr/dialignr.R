04266f354349bdb8b4ed3f7ef9ff392699ce1eb4
npinter <npinter@protonmail.com>
2020-12-28 00:07:55 +0100

    Add dialignr.R script

---
+++ tools/dialignr/dialignr.R
@@ -0,0 +1,13 @@
+library(DIAlignR)
+
+args <- commandArgs(trailingOnly=FALSE)
+
+data_path <- paste0(args[7], "/data/")
+runs_file_path <- paste0(args[7], "/runs.txt")
+
+runs <- scan(runs_file_path, what="", sep="\n")
+
+alignTargetedRuns(dataPath = data_path,
+                  outFile = "alignedTargetedRuns.csv",
+                  runs = runs,
+                  oswMerged = TRUE)
