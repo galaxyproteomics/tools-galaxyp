a2cd91641519a51cfbab33b1c0e3ff83cf58a0ac
Matthias Bernt <m.bernt@ufz.de>
2023-09-12 16:26:14 +0200

    fix python linting

--- tools/calisp/benchmarking.py
+++ tools/calisp/benchmarking.py
@@ -1,8 +1,8 @@
 import argparse
 import os
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 # Define the ArgumentParser
 parser = argparse.ArgumentParser("List of natural abundances of the isotopes")
